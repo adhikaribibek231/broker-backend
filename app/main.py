8267fb14b392db55b230553fbe44bcb091bf0bef
adhikaribibek231 <adhikaribibek231@gmail.com>
2026-03-29 19:57:25 +0545

    feat(deploy):updated cors origin allowed with frontend url

--- app/main.py
+++ app/main.py
@@ -56,6 +56,7 @@ def create_app() -> FastAPI:
         allow_origins=[
             "http://localhost:3000",
             "http://127.0.0.1:3000",
+            "https://broker-frontend.vercel.app/",
         ],
         allow_credentials=True,
         allow_methods=["*"],
